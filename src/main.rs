5a0949717ee8a940e6dc16ece4fb5528c996fb31
shakakibara <aryan.1843@disroot.org>
2025-12-20 01:31:23 +0530

    Use write() instead

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use chrono::{Datelike, Local};
-use std::{fs::File, io::Write, path::PathBuf};
+use std::{fs::write, path::PathBuf};
 
 // TODO: Optimize chrono crate, so that less dependencies are pulled.
 
@@ -15,15 +15,14 @@ fn main() -> std::io::Result<()> {
     // use PathBuf instead of plain format!. Maybe this is the better way?
     let note_path = PathBuf::from("/home/shaka/Documents/notes/毎日/").join(&note_today);
 
-    let mut file = File::create(note_path)?;
-
     // Look if the `%Z` can be configured to show the timezone in string like "IST"
     // as compared to numeric like "05:30"
 
     let formatted_date = now.format("%a %b %d %I:%M:%S %p %Z %Y").to_string();
 
     let contents = format!("# {} \n\n ## Intentions \n\n ## Logs \n\n", formatted_date);
-    file.write_all(contents.as_bytes())?;
+
+    write(&note_path, contents)?;
 
     Ok(())
 }
---
+++ todo.md
@@ -0,0 +1,7 @@
+# TODO
+
+* [ ] Open the current note in neovim.
+  * [ ] Add a check if the file exists, don't overwrite it
+    - This allows that we don't accidently delete our note.
+  * [ ] Clean up chrono crate, minimize dependencies
+  * [ ] Check if using protobuf have any use for us, or it's just another dependency.
